32447b1408e0eb82fdb6d93d2a23e6894c455fcc
Theliraan <theliraan@mail.ru>
2022-02-08 01:47:38 +0300

    Updated asserts, prepared test restore

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -11,42 +11,37 @@ async function execute(command: string, input: string, expected: string) {
 	
 	await commands.executeCommand('editor.action.selectAll');
 	await commands.executeCommand(command);
-	assert.equal(editor.document.getText(), expected);
+	assert.strictEqual(editor.document.getText(), expected);
 
 	await commands.executeCommand('workbench.action.closeActiveEditor');
 }
 
-// suite('Simple Accents Test Suite', () => {
-// 	vscode.window.showInformationMessage('Start tests.');
-
-// 	test('Default settings test', () => {
-// 		const symbols = staticStorage.symbols;
-// 		assert.ok(symbols, 'Default replace symbols array is broken');
-// 		assert.notEqual(symbols.length, 0, 'Default replace symbols array is empty');
-
-// 		symbols.forEach(replaceElement => {
-// 			assert.notEqual(replaceElement.length, 0, 'Replace symbols line is invalid: ' + replaceElement.join(', '));
-// 			assert.notEqual(replaceElement.length, 1, 'Replace symbols line is invalid: ' + replaceElement.join(', '));
-// 		});
-// 	});
-
-// 	const unmodifiedSymbols = 'БВГДЖЗКЛМНПРСТФХЦЧШЩ BCDFGHJKLMNPQRSTVWXZ';
-// 	const sourceSymbols = 'АаЕеИиОоУуЫыЭэЮюЯя AaEeIiOoUuYy -\"';
-// 	const targetSymbols = 'А́а́Е́е́И́и́О́о́У́у́Ы́ы́Э́э́Ю́ю́Я́я́ ÁáÉéÍíÓóÚúÝý —«»';
-
-// 	test('Accentuation test', async () => {
-// 		await execute(
-// 			'simpleaccents.accentuate', 
-// 			unmodifiedSymbols + sourceSymbols, 
-// 			unmodifiedSymbols + targetSymbols
-// 		);
-// 	});
-
-// 	test('Deaccentuation test', async () => {
-// 		await execute(
-// 			'simpleaccents.deaccentuate', 
-// 			unmodifiedSymbols + targetSymbols, 
-// 			unmodifiedSymbols + sourceSymbols
-// 		);
-// 	});
-// });
+/* suite('Simple Accents Test Suite', () => {
+	window.showInformationMessage('Start tests.');
+	test('Default settings test', () => {
+		const symbols = staticStorage.symbolCycles;
+		assert.ok(symbols, 'Default replace symbols array is broken');
+		assert.notStrictEqual(symbols.length, 0, 'Default replace symbols array is empty');
+		symbols.forEach(replaceElement => {
+			assert.notStrictEqual(replaceElement.length, 0, 'Replace symbols line is invalid: ' + replaceElement.join(', '));
+			assert.notStrictEqual(replaceElement.length, 1, 'Replace symbols line is invalid: ' + replaceElement.join(', '));
+		});
+	});
+	const unmodifiedSymbols = 'БВГДЖЗКЛМНПРСТФХЦЧШЩ BCDFGHJKLMNPQRSTVWXZ';
+	const sourceSymbols = 'АаЕеИиОоУуЫыЭэЮюЯя AaEeIiOoUuYy -\"';
+	const targetSymbols = 'А́а́Е́е́И́и́О́о́У́у́Ы́ы́Э́э́Ю́ю́Я́я́ ÁáÉéÍíÓóÚúÝý —«»';
+	test('Accentuation test', async () => {
+		await execute(
+			'simpleaccents.accentuate', 
+			unmodifiedSymbols + sourceSymbols, 
+			unmodifiedSymbols + targetSymbols
+		);
+	});
+	test('Deaccentuation test', async () => {
+		await execute(
+			'simpleaccents.deaccentuate', 
+			unmodifiedSymbols + targetSymbols, 
+			unmodifiedSymbols + sourceSymbols
+		);
+	});
+}); */
